62e6deb5f5c7eb315ce0440cb78111a4ff1b5f8f
Milos Cubrilo <cubrilo.milos@gmail.com>
2021-04-11 20:59:27 +0200

    Removed unusable code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,27 @@ import Block from 'blockchain/block';
 import Blockchain from 'blockchain/blockchain';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'blockchainjs';
-  difficulty = 1;
-  difficultyMessage = '';
+	title = 'blockchainjs';
+	difficulty = 1;
+	difficultyMessage = '';
 
-  onDifficultyChange = (e: any) => {
-    this.difficulty = +e.target.value;
-    this.difficultyMessage = 'Check console for result. \n';
+	onDifficultyChange = (e: any) => {
+		this.difficulty = +e.target.value;
+		this.difficultyMessage = 'Check console for result. \n';
 
-    if (this.difficulty > 4) {
-      this.difficultyMessage += 'If difficulty is greater than 4 mining might take long time. In this case restart browser.';
-    }
-  }
+		if (this.difficulty > 4) {
+			this.difficultyMessage += 'If difficulty is greater than 4 mining might take long time. In this case restart browser.';
+		}
+	}
 
-  executeBlockchain = (): void => {
-    console.log(this.difficulty);
-    const blockchain = new Blockchain(this.difficulty);
+	executeBlockchain = (): void => {
+		console.log(this.difficulty);
+		const blockchain = new Blockchain(this.difficulty);
 
-    blockchain.addBlock(new Block(new Date(), { coin: 3 }));
-    blockchain.addBlock(new Block(new Date(), { coin: 53 }));
-  }
+	}
 }
